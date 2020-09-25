baba1d08eead696b06b32cfeb95291702d527ff2
RyutoKawasaki <various.use.0416@gmail.com>
2020-09-26 00:50:43 +0900

    add specification

--- src/main/kotlin/com/example/idddchatroom/core/domain/message/MessageText.kt
+++ src/main/kotlin/com/example/idddchatroom/core/domain/message/MessageText.kt
@@ -7,6 +7,21 @@ import com.example.idddchatroom.dddFoundation.ValueObject
  * メッセージテキスト
  */
 class MessageText(private val value: String): ValueObject<MessageText.DTO> {
+    companion object {
+        const val MIN_LENGTH = 1
+        const val MAX_LENGTH = 1000
+    }
+
+    init {
+        validateFields()
+    }
+
+    private fun validateFields() {
+        require(value.length in MIN_LENGTH..MAX_LENGTH) {
+            "$value is invalid in ${this::class}"
+        }
+    }
+
     override fun toDTO(): DTO = DTO(value = value)
 
     data class DTO(val value: String)

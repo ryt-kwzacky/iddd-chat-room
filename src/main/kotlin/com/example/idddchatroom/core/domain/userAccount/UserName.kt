7a106b7b91695b6a9fde1551b3a576338d6b8118
RyutoKawasaki <various.use.0416@gmail.com>
2020-09-26 00:34:27 +0900

    add cross-cutting-concern

--- src/main/kotlin/com/example/idddchatroom/core/domain/userAccount/UserName.kt
+++ src/main/kotlin/com/example/idddchatroom/core/domain/userAccount/UserName.kt
@@ -7,6 +7,21 @@ import com.example.idddchatroom.dddFoundation.ValueObject
  * ユーザー名
  */
 class UserName(private val value: String): ValueObject<UserName.DTO> {
+    companion object {
+        const val MIN_LENGTH = 1
+        const val MAX_LENGTH = 16
+    }
+
+    init {
+        validateFields()
+    }
+
+    private fun validateFields() {
+        require(value.length in MIN_LENGTH..MAX_LENGTH) {
+            "$value is invalid in ${this::class}"
+        }
+    }
+
     override fun toDTO(): DTO = DTO(value = value)
 
     data class DTO(val value: String)

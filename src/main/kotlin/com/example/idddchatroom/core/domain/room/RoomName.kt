6c48756853951496f7c9be92de6bb430ada40566
RyutoKawasaki <various.use.0416@gmail.com>
2020-09-26 00:46:01 +0900

    add RoomName VO

--- src/main/kotlin/com/example/idddchatroom/core/domain/room/RoomName.kt
+++ src/main/kotlin/com/example/idddchatroom/core/domain/room/RoomName.kt
@@ -2,14 +2,10 @@ package com.example.idddchatroom.core.domain.room
 
 import com.example.idddchatroom.dddFoundation.ValueObject
 
-/**
- * ユビキタス言語:
- * ルームレベル
- */
-class RoomLevel(private val value: Int): ValueObject<RoomLevel.DTO> {
+class RoomName(private val value: String): ValueObject<RoomName.DTO> {
     companion object {
-        const val MIN_NUMBER = 0
-        const val MAX_NUMBER = 100
+        const val MIN_LENGTH = 1
+        const val MAX_LENGTH = 16
     }
 
     init {
@@ -17,11 +13,11 @@ class RoomLevel(private val value: Int): ValueObject<RoomLevel.DTO> {
     }
 
     private fun validateFields() {
-        require(value in MIN_NUMBER..MAX_NUMBER) {
+        require(value.length in MIN_LENGTH..MAX_LENGTH) {
             "$value is invalid in ${this::class}"
         }
     }
     override fun toDTO(): DTO = DTO(value = value)
 
-    data class DTO(val value: Int)
+    data class DTO(val value: String)
 }

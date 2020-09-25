2c2b279e31ee82119b74e1d05762e132cacbf9bf
RyutoKawasaki <various.use.0416@gmail.com>
2020-09-26 00:38:30 +0900

    add validate for images

--- src/main/kotlin/com/example/idddchatroom/core/domain/message/AttachedImage.kt
+++ src/main/kotlin/com/example/idddchatroom/core/domain/message/AttachedImage.kt
@@ -1,13 +1,26 @@
 package com.example.idddchatroom.core.domain.message
 
+import com.example.idddchatroom.crossCuttingConcern.file.FileStoragePathDefinition
 import com.example.idddchatroom.dddFoundation.ValueObject
 
 /**
  * ユビキタス言語:
  * 添付画像
  */
-class AttachedImage(private val value: String): ValueObject<AttachedImage.DTO> {
-    override fun toDTO(): DTO = DTO(value = value)
+class AttachedImage(private val path: String): ValueObject<AttachedImage.DTO> {
+    companion object {
+        const val PATH_FORMAT = FileStoragePathDefinition.Message.AttachedImage.filePathFormat
+    }
 
-    data class DTO(val value: String)
+    init {
+        validateState()
+    }
+
+    private fun validateState() {
+        require(PATH_FORMAT.toRegex().matches(path)) { "$path is invalid path format." }
+    }
+
+    override fun toDTO(): DTO = DTO(path = path)
+
+    data class DTO(val path: String)
 }
--- src/main/kotlin/com/example/idddchatroom/core/domain/userAccount/IconImage.kt
+++ src/main/kotlin/com/example/idddchatroom/core/domain/userAccount/IconImage.kt
@@ -1,13 +1,26 @@
 package com.example.idddchatroom.core.domain.userAccount
 
+import com.example.idddchatroom.crossCuttingConcern.file.FileStoragePathDefinition
 import com.example.idddchatroom.dddFoundation.ValueObject
 
 /**
  * ユビキタス言語:
  * アイコンイメージ
  */
-class IconImage(private val value: String): ValueObject<IconImage.DTO> {
-    override fun toDTO(): DTO = DTO(value = value)
+class IconImage(private val path: String): ValueObject<IconImage.DTO> {
+    companion object {
+        const val PATH_FORMAT = FileStoragePathDefinition.UserAccount.IconImage.filePathFormat
+    }
 
-    data class DTO(val value: String)
+    init {
+        validateState()
+    }
+
+    private fun validateState() {
+        require(PATH_FORMAT.toRegex().matches(path)) { "$path is invalid path format." }
+    }
+
+    override fun toDTO(): DTO = DTO(path)
+
+    data class DTO(val path: String)
 }

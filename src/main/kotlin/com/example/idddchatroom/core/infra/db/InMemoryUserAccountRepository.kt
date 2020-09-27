6fcf6b565904cb13424f2c4e96a534f44b2240f8
RyutoKawasaki <various.use.0416@gmail.com>
2020-09-27 22:32:22 +0900

    add InMemoryUserAccountRepoaitory

---
+++ src/main/kotlin/com/example/idddchatroom/core/infra/db/InMemoryUserAccountRepository.kt
@@ -0,0 +1,46 @@
+package com.example.idddchatroom.core.infra.db
+
+import com.example.idddchatroom.core.domain.userAccount.*
+import com.example.idddchatroom.dddFoundation.FindResult
+import com.example.idddchatroom.sharedKernel.InMemoryBaseRepository
+import com.example.idddchatroom.sharedKernel.RandomIdentityFactory
+
+
+class InMemoryUserAccountRepository : UserAccountRepository {
+    private val repository = InMemoryBaseRepository<UserAccount>()
+
+    fun reset() {
+        repository.reset()
+    }
+
+    fun count() = repository.count()
+
+    override fun nextIdentity(): UniversalUserId = UniversalUserId(RandomIdentityFactory.create())
+
+    override fun findById(id: UniversalUserId): FindResult<UserAccount> =
+        FindResult(
+            repository.findBy { it.toDTO().id == id }
+        )
+
+    override fun store(userAccount: UserAccount) {
+        val dto = userAccount.toDTO()
+        repository.store(
+            UserAccount(
+                id = dto.id,
+                userName = UserName(dto.userName.value),
+                iconImage = IconImage(dto.icon.path)
+            )
+        )
+    }
+
+    override fun remove(userAccount: UserAccount) {
+        val dto = userAccount.toDTO()
+        repository.remove(
+            UserAccount(
+                id = dto.id,
+                userName = UserName(dto.userName.value),
+                iconImage = IconImage(dto.icon.path)
+            )
+        )
+    }
+}

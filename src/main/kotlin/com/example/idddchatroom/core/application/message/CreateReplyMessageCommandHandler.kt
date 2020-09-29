d431256e7b9dd566465f04368094d867ecc5f38f
Ryuto Kawasaki <37360801+ryt-kwzacky@users.noreply.github.com>
2020-09-29 10:44:37 +0900

    Add CreateReplyMessageCommandHandler & Tests (#18)
    * add CreateReplyMessageCommandHandler
    
    * add CreateReplyMessageCommandHandlerTests

--- src/main/kotlin/com/example/idddchatroom/core/application/message/CreateReplyMessageCommandHandler.kt
+++ src/main/kotlin/com/example/idddchatroom/core/application/message/CreateReplyMessageCommandHandler.kt
@@ -1,26 +1,34 @@
 package com.example.idddchatroom.core.application.message
 
-import com.example.idddchatroom.core.application.message.command.CreateMessageCommand
+import com.example.idddchatroom.core.application.message.command.CreateReplyMessageCommand
 import com.example.idddchatroom.core.domain.message.Message
 import com.example.idddchatroom.core.domain.message.MessageId
 import com.example.idddchatroom.core.domain.message.MessageRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.lang.IllegalStateException
 
 //@Service
-class CreateMessageCommandHandler(
+class CreateReplyMessageCommandHandler(
     @Autowired private val messageRepository: MessageRepository
 ) {
     fun handle(
-        command: CreateMessageCommand
+        command: CreateReplyMessageCommand
     ): MessageId {
+        // TODO: 仕様クラスなどに切り分ける
+        // リプライメッセージにはリプライしようとすると例外を投げる処理
+        val targetMessage = messageRepository.findById(command.targetMessageId).getOrFail()
+        if (targetMessage.isReplyMessage()) {
+            throw IllegalStateException()
+        }
+
         val newMessage = Message.create(
             id = messageRepository.nextIdentity(),
             text = command.text,
             image = command.image,
             roomId = command.roomId,
             sender = command.sender,
-            targetMessageId = null
+            targetMessageId = command.targetMessageId
         )
         messageRepository.store(newMessage)
         return newMessage.id
--- src/main/kotlin/com/example/idddchatroom/core/application/message/command/CreateReplyMessageCommand.kt
+++ src/main/kotlin/com/example/idddchatroom/core/application/message/command/CreateReplyMessageCommand.kt
@@ -4,23 +4,26 @@ import com.example.idddchatroom.core.domain.message.*
 import com.example.idddchatroom.core.domain.room.RoomId
 import com.example.idddchatroom.core.domain.userAccount.UniversalUserId
 
-data class CreateMessageCommand private constructor(
+data class CreateReplyMessageCommand private constructor(
     val text: MessageText,
     val image: AttachedImage,
     val roomId: RoomId,
-    val sender: MessageSender
+    val sender: MessageSender,
+    val targetMessageId: MessageId
 ) {
     companion object {
         fun create(
             text: String,
             image: String,
             roomId: String,
-            sender: String
-        ) = CreateMessageCommand(
+            sender: String,
+            targetMessageId: String
+        ) = CreateReplyMessageCommand(
             text = MessageText(text),
             image = AttachedImage(image),
             roomId = RoomId(roomId),
-            sender = MessageSender(UniversalUserId(sender))
+            sender = MessageSender(UniversalUserId(sender)),
+            targetMessageId = MessageId(targetMessageId)
         )
     }
 }

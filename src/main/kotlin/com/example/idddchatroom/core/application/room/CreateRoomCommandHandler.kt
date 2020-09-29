af8b42b61d161ab13c01a5cff693480424676cef
Ryuto Kawasaki <37360801+ryt-kwzacky@users.noreply.github.com>
2020-09-30 02:40:55 +0900

    Add specification to CreateRoomCommandHandler (#22)
    * fix application service

--- src/main/kotlin/com/example/idddchatroom/core/application/room/CreateRoomCommandHandler.kt
+++ src/main/kotlin/com/example/idddchatroom/core/application/room/CreateRoomCommandHandler.kt
@@ -1,19 +1,32 @@
 package com.example.idddchatroom.core.application.room
 
 import com.example.idddchatroom.core.application.room.command.CreateRoomCommand
+import com.example.idddchatroom.core.domain.message.MessageRepository
 import com.example.idddchatroom.core.domain.room.Room
 import com.example.idddchatroom.core.domain.room.RoomId
 import com.example.idddchatroom.core.domain.room.RoomRepository
+import com.example.idddchatroom.core.domain.userAccount.UniversalUserId
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 //@Service
 class CreateRoomCommandHandler(
-    @Autowired private val roomRepository: RoomRepository
+    @Autowired private val roomRepository: RoomRepository,
+    @Autowired private val messageRepository: MessageRepository
 ) {
     fun handle(
         command: CreateRoomCommand
     ): RoomId {
+        // TODO: ロール与えてAOPで処理するようにする
+        // TODO: 複雑なのでspecificationクラスを用意
+        // 一つ以上メッセージを送っていることをチェック
+        if (!messageRepository.findAllByUniversalUserId(command.ownerId.toDTO().value).exists()) {
+            // 一つ以上ルームを作成していることをチェック
+            if (roomRepository.findAllByRoomOwner(command.ownerId).exists()) {
+                throw IllegalStateException()
+            }
+        }
+
         val newRoom = Room.create(
             roomId = roomRepository.nextIdentity(),
             roomName = command.roomName,
